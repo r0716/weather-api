1f26a63d4d7e61c343c4f67d129ea0da04527f39
Rianna Winston <riannawinston@macbook-air.myfiosgateway.com>
2023-07-04 00:07:33 -0400

    fixing inputs and styling

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# weather-api
+Open Weather API
+
+
+This repository contains code for interacting with the Open Weather API to fetch current weather data for a given city.
+
+Before getting started, you need to create an account at WeatherAPI to obtain an API key.
+
+Open the index.html file in your preferred web browser.
+
+Enter the desired city in the provided text input.
+
+Click the "Submit" button to fetch the weather data for the specified city.
+
+The weather information will be displayed on the screen, including the city name and the temperature.
--- app.js
+++ app.js
@@ -1,24 +1,28 @@
-
 const apiKey = 'e5561d2967814183afd194440230307'
 const button = document.querySelector('#submitButton')
 const input = document.querySelector('#textInput')
 const city = document.querySelector('#cityName')
 const temp = document.querySelector('#temp')
 
-button.addEventListener('click', () =>{
+
+button.addEventListener('click', () => {
     let inputValue = input.value;
-    city.innerHTML = inputValue; // Update the cityName with the value of inputValue
-    temp.innerHTML = '' //Update the city with the value of the temp from weatherapi    
+    city.innerHTML = `City: ${inputValue}`; // Update the cityName with the value of inputValue
+    temp.innerHTML = ''; // Clear the previous temperature value
+  
+    (async () => {
+      try {
+        const response = await axios.get(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${inputValue}&aqi=no`);
+        console.log(response.data);
+  
+        // Update the temp element with the fetched temperature in Fahrenheit & Celcius
+        temp.textContent = `Current Temp: ${response.data.current.temp_f} Fahrenheit`;
+      } catch (error) {
+        console.log(error);
+        temp.innerHTML = 'Problem fetching weather data. Please try a different city.';
 
-    // console.log(city.innerHTML);
-    // console.log(inputValue);
-    toggle();
-
-
-});
-
-
-function toggle (){
-    button.classList.toggle('altColor')
-}
-
+      }
+  
+    })();
+  });
+  
