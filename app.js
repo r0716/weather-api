c0e4f81257de07592e3dc6e850b30de387c9d7c6
Rianna Winston <riannawinston@macbook-air.myfiosgateway.com>
2023-07-03 21:56:11 -0400

    adding input fuctionality

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const apiKey = 'e5561d2967814183afd194440230307'
 const button = document.querySelector('#submitButton')
 const input = document.querySelector('#textInput')
 const city = document.querySelector('#cityName')
+const temp = document.querySelector('#temp')
 
 button.addEventListener('click', () =>{
     let inputValue = input.value;
-    let cityValue = city.textContent;
+    city.innerHTML = inputValue; // Update the cityName with the value of inputValue
+    temp.innerHTML = '' //Update the city with the value of the temp from weatherapi    
 
-    console.log(cityValue);
-    console.log(inputValue)
-    toggle()
+    // console.log(city.innerHTML);
+    // console.log(inputValue);
+    toggle();
+
 
 });
 
